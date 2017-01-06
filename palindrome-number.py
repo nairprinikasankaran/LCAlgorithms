e930af87e813d59945c48b6fe301a1944b25712d
nairprinikasankaran <nairprinikasankaran@gmail.com>
2017-01-06 00:59:07 -0800

    Create palindrome-number.py

---
+++ palindrome-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            x * -1
+        string = str(x)
+        start = 0
+        end = len(string) - 1
+        while start <= end:
+            if string[start] != string[end]:
+                return False
+            else:
+                start += 1
+                end -= 1
+        return True
